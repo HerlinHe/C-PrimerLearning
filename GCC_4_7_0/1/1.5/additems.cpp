c6ece0241a50d0a7fbf35b37f27a075b0681a5fb
何汉琳 <herlin0793@gmail.com>
2022-08-03 11:03:16 -0700

    add log

--- GCC_4_7_0/1/1.5/additems.cpp
+++ GCC_4_7_0/1/1.5/additems.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
+#include <fstream>
 #include "../Sales_item.h"
 using namespace std;
 
 int main()
 {
+    ofstream logFile("logfile");
+    clog.rdbuf(logFile.rdbuf());
     Sales_item item1, item2;
     cin >> item1 >> item2;
-    cout << item1 + item2 << endl;
+    if (item1.isbn() == item2.isbn()) {
+        cout << item1 + item2 << endl;
+        clog << "Successfully add two items" << endl;
+    }
+    else
+        clog << "Input items cannot be added" << endl;
+
     return 0;
 }
